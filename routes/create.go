b35137f40dc6ebdd12f50c9cdb5e3918ea38c524
Marcel Baur <marcel@baur.gg>
2022-03-14 18:39:19 +0100

    refactor: use constants for recurrent response keys

--- routes/create.go
+++ routes/create.go
@@ -13,15 +13,15 @@ type NewGame struct {
 func CreateGameCaller(c *gin.Context) {
     var request NewGame
     if err := c.ShouldBindJSON(&request); err != nil {
-        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        c.JSON(http.StatusBadRequest, gin.H{ERROR_KEY: err.Error()})
         return
     }
     if request.Letters < 4 {
-        c.JSON(http.StatusBadRequest, gin.H{"error": "Need at least four letters!"})
+        c.JSON(http.StatusBadRequest, gin.H{ERROR_KEY: "Need at least four letters!"})
         return
     }
     if request.Letters > 7 {
-        c.JSON(http.StatusBadRequest, gin.H{"error": "Not more than seven letters allowed"})
+        c.JSON(http.StatusBadRequest, gin.H{ERROR_KEY: "Not more than seven letters allowed"})
         return
     }
     game := logic.CreateGame(request.Letters)
