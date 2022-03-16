90f0a28aa9fe2b22c2128496134c304cbe14f083
Marcel Baur <marcel@baur.gg>
2022-03-16 17:50:42 +0100

    refactor: run gofmt

--- main.go
+++ main.go
@@ -1,45 +1,45 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-    "log"
-    "os"
-    "bufio"
-	"github.com/marcel-baur/wrdl/routes"
-    "github.com/marcel-baur/wrdl/res"
-    "github.com/marcel-baur/wrdl/logic"
 	"github.com/gin-gonic/gin"
+	"github.com/marcel-baur/wrdl/logic"
+	"github.com/marcel-baur/wrdl/res"
+	"github.com/marcel-baur/wrdl/routes"
+	"log"
+	"os"
 )
 
 func main() {
-    fmt.Printf("Starting server on port 8080")
-    gin.ForceConsoleColor()
-    fmt.Printf("Loading words...")
-    LoadWords()
-    logic.GMap = make(logic.GameMap)
-    fmt.Printf("Words loaded! %d words in list.", len(res.EnglishWords))
-    r := gin.Default()
-    r.POST("/new", routes.CreateGameCaller)
-    r.POST("/check", routes.PostSolutionCaller)
-    r.Run()
+	fmt.Printf("Starting server on port 8080")
+	gin.ForceConsoleColor()
+	fmt.Printf("Loading words...")
+	LoadWords()
+	logic.GMap = make(logic.GameMap)
+	fmt.Printf("Words loaded! %d words in list.", len(res.EnglishWords))
+	r := gin.Default()
+	r.POST("/new", routes.CreateGameCaller)
+	r.POST("/check", routes.PostSolutionCaller)
+	r.Run()
 }
 
 func LoadWords() {
-    f, err := os.Open("res/words.txt")
+	f, err := os.Open("res/words.txt")
 
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    defer f.Close()
+	defer f.Close()
 
-    scanner := bufio.NewScanner(f)
+	scanner := bufio.NewScanner(f)
 
-    for scanner.Scan() {
-        res.EnglishWords = append(res.EnglishWords, scanner.Text())
-    }
+	for scanner.Scan() {
+		res.EnglishWords = append(res.EnglishWords, scanner.Text())
+	}
 
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
-    }
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
--- routes/solve.go
+++ routes/solve.go
@@ -1,32 +1,32 @@
 package routes
 
 import (
-    "net/http"
-    "fmt"
-    "github.com/gin-gonic/gin"
-    "github.com/google/uuid"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/marcel-baur/wrdl/logic"
+	"net/http"
 )
 
 type PostSolveRequest struct {
-    Hash uuid.UUID
-    Word string
+	Hash uuid.UUID
+	Word string
 }
 
 func PostSolutionCaller(c *gin.Context) {
-    var request PostSolveRequest
+	var request PostSolveRequest
 
-    if err := c.ShouldBindJSON(&request); err != nil {
-        c.JSON(http.StatusBadRequest, gin.H{ERROR_KEY: err.Error()})
-        return
-    }
+	if err := c.ShouldBindJSON(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{ERROR_KEY: err.Error()})
+		return
+	}
 
-    game := logic.GMap[request.Hash]
-    if game == nil {
-        c.JSON(http.StatusBadRequest, gin.H{ERROR_KEY: "Game not found!"})
-    }
-    if len(request.Word) != len(game.Solution) {
-        err_string := fmt.Sprintf("The proposed solution must have length %d", len(game.Solution))
-        c.JSON(http.StatusBadRequest, gin.H{ERROR_KEY: err_string})
-    }
+	game := logic.GMap[request.Hash]
+	if game == nil {
+		c.JSON(http.StatusBadRequest, gin.H{ERROR_KEY: "Game not found!"})
+	}
+	if len(request.Word) != len(game.Solution) {
+		err_string := fmt.Sprintf("The proposed solution must have length %d", len(game.Solution))
+		c.JSON(http.StatusBadRequest, gin.H{ERROR_KEY: err_string})
+	}
 }
